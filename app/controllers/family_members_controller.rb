4dd71e842ba19445a728a9ed19dbd48a86a7402e
Sara Bilmes <saraesrig@gmail.com>
2017-10-19 12:36:24 +0300

    added family-member-controller

---
+++ app/controllers/family_members_controller.rb
@@ -0,0 +1,19 @@
+class FamilyMembersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'family_members/index'
+
+  get 'family_members/new'
+
+  get 'family_members/create'
+
+  get 'family_members/show'
+
+  get 'family_members/edit'
+
+  get 'family_members/delete'
+
   devise_for :users
   root to: 'home#index'
 end
